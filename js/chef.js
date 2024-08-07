03ecddeadf3b0c8275f299a0f72979bb54666841
Pratik Thapa <PratikThapa288@gmail.com>
2024-08-06 21:04:14 -0400

    js

---
+++ js/chef.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function() {
+const chefImage = document.querySelector('.chef-image');
+
+setTimeout(function() {
+    chefImage.style.opacity = '0';
+}, 5000);
+});
